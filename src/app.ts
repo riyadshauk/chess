19163c910eb19121f88907442a6a7a1f8ceaa6ab
Riyad Shauk <riyad.shauk@gmail.com>
2020-06-15 23:54:01 -0700

    fix css bug in firefox, minor fixes.

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,5 @@
 import Controller from './controller/controller';
+import { qs } from './view/helpers';
 const controller = new Controller;
+// @ts-ignore
+qs('.chessboard > board:nth-child(1) > tr:nth-child(7) > td:nth-child(5) > div:nth-child(1)').click();
